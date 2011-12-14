6955bf8df89acf18786ac5e7c5e9246c86d903b3
Patryk Acewicz <pacewiczhepfner@sigma.ug.edu.pl>
2011-12-14 03:56:16 +0100

    extended version v1

--- UGNames.sh
+++ UGNames.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 if [ -z "$1" ]
 then
-  echo "Usage: `basename $0` arg"
-  echo "   arg - can be a part of a name, surname or a degree you want to search for (case-insensitive)"
+  echo "Usage: `basename $0` arg [s]"
+  echo "   arg - can be a part of a name, surname or a degree to search for"
+  echo "   s   - if given, the search will be case-sensitive"
   exit 1
 fi
-wget -O- -q http://inf.ug.edu.pl/plan/pracownicy.php | egrep -e '<nobr>[^<]*</nobr>' -o | tac  | sed -e '{
+int=$(wget -O- -q http://inf.ug.edu.pl/plan/pracownicy.php | egrep -e '<nobr>[^<]*</nobr>' -o | tac  | sed -e '{
 N
 N
-s/\n/ /g}' -e 's/<[^>]*>//g' | grep -i -e "$1"
+s/\n/ /g}' -e 's/<[^>]*>//g');
+if [ -z "$2" ]
+then
+echo "$int" | grep -i -e "$1";
+else
+echo "$int" | grep -e "$1";
+fi
